f7e198d41ddf4f7ccf2ab82b0877f2145c1f6ed5
Vlad <git.vladislav.kolomytsev@gmail.com>
2022-12-08 16:54:01 +0300

    Add model

--- user-info-service/src/main/java/com/market/userinfoservice/model/User.java
+++ user-info-service/src/main/java/com/market/userinfoservice/model/User.java
@@ -11,7 +11,6 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
