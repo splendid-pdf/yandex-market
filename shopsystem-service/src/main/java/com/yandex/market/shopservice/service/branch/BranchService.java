732b24a6d715abc2bde54dfcaf2252ee7f184f95
DameerGamlet <gubaidulin.dam@gmail.com>
2022-12-24 00:15:06 +0400

    Добавил возможность обновления отдела по внешнему ID. Также незначитальные изменения в наименованиях

--- shopsystem-service/src/main/java/com/yandex/market/shopservice/service/branch/BranchService.java
+++ shopsystem-service/src/main/java/com/yandex/market/shopservice/service/branch/BranchService.java
@@ -1,7 +1,6 @@
 package com.yandex.market.shopservice.service.branch;
 
 import com.yandex.market.shopservice.dto.branch.BranchDto;
-import com.yandex.market.shopservice.model.branch.Branch;
 
 import java.util.UUID;
 
