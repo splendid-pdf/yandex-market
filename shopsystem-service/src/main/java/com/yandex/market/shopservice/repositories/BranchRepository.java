5750f9b152c24eaf47f5530197f65eefa516a856
DameerGamlet <gubaidulin.dam@gmail.com>
2022-12-23 19:47:29 +0400

    Добавил возможность обновления отдела по внешнему ID. Также незначитальные изменения в наименованиях

--- shopsystem-service/src/main/java/com/yandex/market/shopservice/repositories/BranchRepository.java
+++ shopsystem-service/src/main/java/com/yandex/market/shopservice/repositories/BranchRepository.java
@@ -1,7 +1,6 @@
 package com.yandex.market.shopservice.repositories;
 
 import com.yandex.market.shopservice.model.branch.Branch;
-import com.yandex.market.shopservice.model.shop.ShopSystem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +9,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface BranchRepository extends JpaRepository<Branch, Long> {
-    @Query(value = "FROM Branch ss " +
-            "WHERE ss.externalId=:externalId")
+    @Query(value = "FROM Branch b " +
+            "WHERE b.externalId=:externalId AND b.pickup = false")
     Optional<Branch> findByExternalId(@Param("externalId") UUID externalId);
 }
