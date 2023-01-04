4f73be79b5d1795904b14733a4e134c23a8ece74
DameerGamlet <gubaidulin.dam@gmail.com>
2023-01-04 21:16:56 +0400

    Added the ability to view branches by externalId Shop System

--- shopsystem-service/src/test/java/com/example/shopservice/ShopServiceApplicationTests.java
+++ shopsystem-service/src/test/java/com/example/shopservice/ShopServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.shopservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ShopServiceApplicationTests.class)
 class ShopServiceApplicationTests {
 
     @Test
