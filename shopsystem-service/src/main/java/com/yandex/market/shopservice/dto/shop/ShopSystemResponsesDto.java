8c798dbe605fa4161822ad7f04b2792df666d092
DameerGamlet <gubaidulin.dam@gmail.com>
2023-01-14 13:50:28 +0400

    Added the ability to view branches by externalId Shop System

--- shopsystem-service/src/main/java/com/yandex/market/shopservice/dto/shop/ShopSystemResponsesDto.java
+++ shopsystem-service/src/main/java/com/yandex/market/shopservice/dto/shop/ShopSystemResponsesDto.java
@@ -1,13 +1,7 @@
 package com.yandex.market.shopservice.dto.shop;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.yandex.market.shopservice.dto.LocationDto;
 import com.yandex.market.shopservice.dto.branch.BranchDto;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 import java.util.Set;
@@ -19,28 +13,19 @@ import java.util.Set;
 @EqualsAndHashCode
 @AllArgsConstructor
 public class ShopSystemResponsesDto {
-    @NotBlank(message = "\"Name\" field must not be empty")
     private String name;
 
-    @NotBlank(message = "\"Token\" field must not be empty")
     private String token;
 
-    @Valid
-    @NotNull(message = "\"Support contact of Shop\" field must not be empty")
     private SupportDto support;
 
-    @Valid
-    @NotNull(message = "\"Location of Shop\" field must not be empty")
     private LocationDto legalEntityAddress;
 
-    private Set<@Valid SpecialOfferDto> specialOffers;
+    private Set<SpecialOfferDto> specialOffers;
 
-    private Set<@Valid BranchDto> branches;
+    private Set<BranchDto> branches;
 
-    @Pattern(regexp = "^(https?:\\/\\/)?([\\w-]{1,32}\\.[\\w-]{1,32})[^\\s@]*$",
-            message = "Invalid \"Logo URL\" entered")
     private String logoUrl;
 
-    @Positive(message = "\"Rating of Shop\" field must not be negative")
     private float rating;
 }
