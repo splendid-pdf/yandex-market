d7e4beec802ae10c7bca4dbe994e9aaa01fd21ca
Arkady Railyanu <eseo.a@yandex.ru>
2023-01-31 19:10:35 +0300

    added util exceptions class

--- product-service/src/main/java/com/yandex/market/productservice/service/ProductService.java
+++ product-service/src/main/java/com/yandex/market/productservice/service/ProductService.java
@@ -4,12 +4,15 @@ import com.yandex.market.productservice.dto.ProductRequestDto;
 import com.yandex.market.productservice.dto.ProductResponseDto;
 import com.yandex.market.productservice.mapper.ProductMapper;
 import com.yandex.market.productservice.repository.ProductRepository;
+import com.yandex.market.productservice.utils.ExceptionMessagesConstants;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
+
+import static com.yandex.market.productservice.utils.ExceptionMessagesConstants.*;
 
 @Service
 @RequiredArgsConstructor
@@ -20,14 +23,14 @@ public class ProductService {
     @Transactional(readOnly = true)
     public ProductResponseDto getProductByExternalId(UUID externalId) {
         Product product = productRepository.findByExternalId(externalId)
-                .orElseThrow(() -> new EntityNotFoundException("ERROR_MESSAGE" + externalId));
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND_ERROR_MESSAGE + externalId));
         return productMapper.toResponseDto(product);
     }
 
     @Transactional
     public void deleteProductByExternalId(UUID externalId) {
         Product product = productRepository.findByExternalId(externalId)
-                .orElseThrow(() -> new EntityNotFoundException("ERROR_MESSAGE" + externalId));
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND_ERROR_MESSAGE + externalId));
         product.setDeleted(true);
     }
 
@@ -40,7 +43,7 @@ public class ProductService {
     @Transactional
     public ProductResponseDto updateProductByExternalId(UUID externalId, ProductRequestDto productRequestDto) {
         Product storedProduct = productRepository.findByExternalId(externalId)
-                .orElseThrow(() -> new EntityNotFoundException("ERROR_MESSAGE" + externalId));
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND_ERROR_MESSAGE + externalId));
         Product updatedProduct = productMapper.toProduct(productRequestDto);
         updatedProduct.setId(storedProduct.getId());
         return productMapper.toResponseDto(productRepository.save(updatedProduct));
