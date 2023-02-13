ebdfec9be3249c3fa6d89d06ceb67ffce11214f4
Arkady Railyanu <eseo.a@yandex.ru>
2023-02-13 03:13:19 +0300

    updated migrations

---
+++ product-service/src/main/resources/db/migration/V3__Drop_old_version_DB.sql
@@ -0,0 +1,9 @@
+drop sequence if exists "category-generator";
+drop sequence if exists "characteristic-generator";
+drop sequence if exists "product_price-generator";
+drop sequence if exists "products-generator";
+
+drop table if exists category cascade;
+drop table if exists characteristics cascade;
+drop table if exists product_price cascade;
+drop table if exists products cascade;
