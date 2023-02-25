b97807b0eaabb0f3d655b38547a177ac91487eac
DameerGamlet <gubaidulin.dam@gmail.com>
2023-02-25 18:31:00 +0400

    List of products and archive. Swagger integration.

--- product-service/src/main/java/com/yandex/market/productservice/config/SpringdocConfig.java
+++ product-service/src/main/java/com/yandex/market/productservice/config/SpringdocConfig.java
@@ -3,6 +3,7 @@ package com.yandex.market.productservice.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,5 +21,13 @@ public class SpringdocConfig {
         return new Info()
                 .title("Spring Doc Open Api Product-service")
                 .version("1.0.0");
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/public/api/v1/**")
+                .build();
     }
 }
