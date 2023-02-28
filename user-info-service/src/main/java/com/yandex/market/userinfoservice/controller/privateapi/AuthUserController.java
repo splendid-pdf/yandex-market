91038ee8ccf5733c17212de29c1ab0f41d1ca71f
Vlad <git.vladislav.kolomytsev@gmail.com>
2023-02-28 18:56:04 +0300

    Some fixes

--- user-info-service/src/main/java/com/yandex/market/userinfoservice/controller/privateapi/AuthUserController.java
+++ user-info-service/src/main/java/com/yandex/market/userinfoservice/controller/privateapi/AuthUserController.java
@@ -2,6 +2,7 @@ package com.yandex.market.userinfoservice.controller.privateapi;
 
 import com.yandex.market.userinfoservice.dto.UserAuthenticationDto;
 import com.yandex.market.userinfoservice.service.UserSearchService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "private")
 @RequiredArgsConstructor
 @RequestMapping("/private/api/v1/users/auth-details")
 public class AuthUserController {
