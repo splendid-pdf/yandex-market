11f0f06938c6c9ea4e3f5ca76511167cf275cd06
zhenyashvyrkov <zhenya100694@mail.ru>
2023-03-14 01:37:13 +0300

    add response header transmitter and fix bug with redis

--- api-gateway-service/src/main/java/com/marketplace/gateway/controller/SignController.java
+++ api-gateway-service/src/main/java/com/marketplace/gateway/controller/SignController.java
@@ -3,6 +3,11 @@ package com.marketplace.gateway.controller;
 import com.marketplace.gateway.dto.OAuthUserResponse;
 import com.marketplace.gateway.dto.UserLoginRequest;
 import com.marketplace.gateway.service.OAuthAuthenticationService;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -16,16 +21,28 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/public/api/v1/oauth")
 @RequiredArgsConstructor
+@ApiResponses(value = {@ApiResponse(responseCode = "401", description = "Unauthorized")})
 public class SignController {
 
     private final OAuthAuthenticationService authenticationService;
 
     @PostMapping("/login")
-    public Mono<OAuthUserResponse> login(@RequestBody Mono<UserLoginRequest> request, ServerWebExchange exchange) {
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully Logged In"),
+    })
+    public Mono<OAuthUserResponse> login(@RequestBody Mono<UserLoginRequest> request,
+                                         ServerWebExchange exchange) {
         return authenticationService.authenticate(request, exchange);
     }
 
     @PostMapping("/logout")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successfully Logged out")
+    })
+    @Parameters(value = {
+            @Parameter(in = ParameterIn.HEADER, name = "Authorization", required = true),
+            @Parameter(in = ParameterIn.HEADER, name = "X-User-Id", required = true)
+    })
     public Mono<ResponseEntity<Void>> logout(ServerHttpRequest request) {
         return authenticationService.deauthenticate(request);
     }
--- api-gateway-service/src/main/java/com/marketplace/gateway/util/AuthConstants.java
+++ api-gateway-service/src/main/java/com/marketplace/gateway/util/AuthConstants.java
@@ -1,5 +1,5 @@
 package com.marketplace.gateway.util;
 
 public class AuthConstants {
-    public static final String X_USER_ID_HEADER = "X-USER-ID";
+    public static final String X_USER_ID_HEADER = "X-User-Id";
 }
