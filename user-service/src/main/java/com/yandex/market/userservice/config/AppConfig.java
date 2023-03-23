df7583f689f41cbde8d18aec8c7860de9eef8708
Vlad <git.vladislav.kolomytsev@gmail.com>
2023-03-23 22:31:54 +0300

    Fix PR issues

--- user-service/src/main/java/com/yandex/market/userservice/config/AppConfig.java
+++ user-service/src/main/java/com/yandex/market/userservice/config/AppConfig.java
@@ -2,10 +2,8 @@ package com.yandex.market.userservice.config;
 
 import com.yandex.market.userservice.config.properties.ErrorInfoProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration(proxyBeanMethods = false)
 public class AppConfig {
@@ -14,10 +12,5 @@ public class AppConfig {
     @ConfigurationProperties(prefix = "app.validation")
     public ErrorInfoProperties errorInfoProperties() {
         return new ErrorInfoProperties();
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplateBuilder().build();
     }
 }
