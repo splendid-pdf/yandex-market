a26122042dc1a4bd376f072f1336ce6f48744f26
DameerGamlet <gubaidulin.dam@gmail.com>
2023-03-23 19:20:34 +0400

    Tests

--- product-service/src/test/java/com/yandex/market/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/yandex/market/productservice/ProductServiceApplicationTests.java
@@ -9,7 +9,6 @@ import com.yandex.market.productservice.mapper.ProductMapper;
 import com.yandex.market.productservice.repository.ProductRepository;
 import com.yandex.market.productservice.service.ProductService;
 import com.yandex.market.util.RestPageImpl;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
@@ -27,19 +26,17 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
-import static com.yandex.market.productservice.utils.ExceptionMessagesConstants.PRODUCT_NOT_FOUND_ERROR_MESSAGE;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -50,7 +47,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 @SqlGroup({
         @Sql(value = "classpath:db/insert_tests_fields.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
-        @Sql(value = "classpath:db/delete_test_date.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+        @Sql(value = "classpath:db/insert_types.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 })
 class ProductServiceApplicationTests {
 
@@ -122,76 +119,6 @@ class ProductServiceApplicationTests {
 
     @Test
     @Disabled
-    void shouldDeleteListProductBySellerIdSuccessDeleted() throws Exception {
-        List<UUID> productIds = new ArrayList<>(List.of(
-                UUID.fromString("301c5370-be41-421e-9b15-f1e80a7074d1"),
-                UUID.fromString("301c5370-be41-421e-9b15-f1e80a7074d2"),
-                UUID.fromString("301c5370-be41-421e-9b15-f1e80a7074d3")));
-
-        executeDelete(DELETE_SELLER_ID, productIds);
-
-        long expectedBeforeDelete = 5 - productIds.size(),
-                actualCountAfterDelete = getActualCountAfterDelete(DELETE_SELLER_ID);
-
-        assertEquals(expectedBeforeDelete, actualCountAfterDelete);
-    }
-
-    /**
-     * Method: shouldDeleteListProductBySellerIdNotAllProductsFound()
-     *
-     * @throws Exception throws out methods {@link ProductServiceApplicationTests#executeDelete(UUID, List)},
-     *                   {@link ProductServiceApplicationTests#getActualCountAfterDelete(UUID)}
-     *                   <br>
-     * @value <b>productIds</b> list elements (0 - exists, 1 - not exists, 2 - not exists)
-     */
-    @Test
-    @Disabled
-    void shouldDeleteListProductBySellerIdNotAllProductsFound() throws Exception {
-        List<UUID> productIds = new ArrayList<>(List.of(
-                UUID.fromString("301c5370-be41-421e-9b15-f1e80a7074d4"),
-                UUID.fromString("301c5370-be41-421e-9b15-f1e80a7074c5"),
-                UUID.fromString("301c5370-be41-421e-9b15-f1e80a7074c6")));
-
-        executeDelete(DELETE_SELLER_ID, productIds);
-
-        int countNotFound = 2;
-        long expectedBeforeDelete = 5 - (productIds.size() - countNotFound),
-                actualCountAfterDelete = getActualCountAfterDelete(DELETE_SELLER_ID);
-
-        assertEquals(expectedBeforeDelete, actualCountAfterDelete);
-    }
-
-    /**
-     * Method: shouldDeleteListProductBySellerIdNotAllProductsCanBeDeleted()
-     *
-     * @throws Exception throws out methods {@link ProductServiceApplicationTests#executeDelete(UUID, List)},
-     *                   {@link ProductServiceApplicationTests#getActualCountAfterDelete(UUID)}
-     *                   <br>
-     * @value <b>productIds</b> list elements (isArchived = false, isArchived = false,
-     * 2 - isArchived = true, 3 - isArchived = false)
-     * @value <b>expectedBeforeDelete</b> expected 5 - (4 - 3 (isArchived = false))
-     */
-    @Test
-    @Disabled
-    void shouldDeleteListProductBySellerIdNotAllProductsCanBeDeleted() throws Exception {
-        List<UUID> productIds = new ArrayList<>(List.of(
-                UUID.fromString("301c5370-be41-421e-9b15-f1e80a7074d5"),
-                UUID.fromString("301c5370-be41-421e-9b15-f1e80a7074d6"),
-                UUID.fromString("301c5370-be41-421e-9b15-f1e80a7074d7"),
-                UUID.fromString("301c5370-be41-421e-9b15-f1e80a7074d8")));
-
-        executeDelete(DELETE_SELLER_ID, productIds);
-
-        int countNotFound = 3;
-
-        long expectedBeforeDelete = 5 - (productIds.size() - countNotFound),
-                actualCountAfterDelete = getActualCountAfterDelete(DELETE_SELLER_ID);
-
-        assertEquals(expectedBeforeDelete, actualCountAfterDelete);
-    }
-
-    @Test
-    @Disabled
     @Transactional
     void shouldCreateProductReturnExternalIdAndStatus201Created() throws Exception {
         UUID sellerExternalId = UUID.fromString("cd8ae5aa-ebea-4922-b3c2-8ba8a296ef04");
@@ -252,83 +179,10 @@ class ProductServiceApplicationTests {
         assertNotNull(objectMapper.readValue(mvcResult.getResponse().getContentAsString(), ErrorResponse.class));
     }
 
-    @Test
-    @Transactional
-    void shouldChangeProductPriceSellerAndProductFoundAndChanged() throws Exception {
-        UUID sellerId = UUID.fromString("37678201-f3c8-4d5c-a628-2344eef50c60");
-        UUID productId = UUID.fromString("37678201-f3c8-4d5c-a628-2344eef50c61");
-        long newPrice = 30L;
-
-        changePrice(sellerId, productId, newPrice, status().isNoContent());
-
-        Long actualPrice = findProductByExternalId(productId).price();
-        assertEquals(newPrice, actualPrice, "Received price not as expected");
-    }
-
-    @Test
-    @Transactional
-    void shouldChangeProductPriceSellerNotFoundAndNotChanged() throws Exception {
-        UUID sellerId = UUID.fromString("37678201-f3c8-4d5c-a628-2344eef50c63");
-        UUID productId = UUID.fromString("37678201-f3c8-4d5c-a628-2344eef50c61");
-        long newPrice = 40L;
-
-        changePrice(sellerId, productId, newPrice, status().isNoContent());
-
-        long actualPrice = findProductByExternalId(productId).price();
-        assertNotEquals(newPrice, actualPrice, "Received values are equal, which is not expected");
-    }
-
-    @Test
-    @Transactional
-    void shouldChangeProductPriceProductNotFoundAndNotChanged() throws Exception {
-        UUID sellerId = UUID.fromString("37678201-f3c8-4d5c-a628-2344eef50c60");
-        UUID productId = UUID.fromString("37678201-f3c8-4d5c-a628-2344eef50c69");
-        long newPrice = 50L;
-
-        changePrice(sellerId, productId, newPrice, status().isNoContent());
-
-        assertThrows(EntityNotFoundException.class, () -> findProductByExternalId(productId),
-                "Product was found by given externalId");
-    }
-
-    private void changePrice(UUID sellerId, UUID productId, long newPrice, ResultMatcher status) throws Exception {
-        mockMvc.perform(patch(
-                        PATH_TO_SELLER + "{sellerId}/products/price/{productId}", sellerId, productId)
-                        .param("newPrice", String.valueOf(newPrice))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status)
-                .andReturn();
-    }
-
-    private ProductResponseDto findProductByExternalId(UUID externalId) {
-        return productMapper.toResponseDto(
-                productRepository
-                        .findByExternalId(externalId)
-                        .orElseThrow(() -> new EntityNotFoundException(PRODUCT_NOT_FOUND_ERROR_MESSAGE + externalId)));
-    }
-
     private Page<ProductResponseDto> getPageOfProductFromMvcResult(MvcResult mvcResult)
             throws UnsupportedEncodingException, JsonProcessingException {
         return objectMapper.readValue(
                 mvcResult.getResponse().getContentAsString(), new TypeReference<RestPageImpl<ProductResponseDto>>() {
                 });
-    }
-
-    private void executeDelete(UUID sellerId, List<UUID> productIds) throws Exception {
-        mockMvc.perform(delete(
-                        PATH_TO_SELLER + "{sellerId}/products", sellerId)
-                        .content(objectMapper.writeValueAsString(productIds))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNoContent());
-    }
-
-    private long getActualCountAfterDelete(UUID sellerId) throws Exception {
-        return objectMapper.readValue(mockMvc.perform(get(
-                        PATH_TO_SELLER + "{sellerId}/products", sellerId, PageRequest.of(0, 20))
-                        .param("method", "ARCHIVE")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString(), new TypeReference<RestPageImpl<ProductResponseDto>>() {
-        }).getTotalElements();
     }
 }
