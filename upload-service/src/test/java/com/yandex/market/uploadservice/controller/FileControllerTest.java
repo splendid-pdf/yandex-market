27f94f89de18b9f1c1c734459191bb37e2dfe89b
Arkady Railyanu <eseo.a@yandex.ru>
2023-04-26 14:48:51 +0300

    edited test

--- upload-service/src/test/java/com/yandex/market/uploadservice/controller/FileControllerTest.java
+++ upload-service/src/test/java/com/yandex/market/uploadservice/controller/FileControllerTest.java
@@ -2,50 +2,33 @@ package com.yandex.market.uploadservice.controller;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.yandex.market.uploadservice.UploadIntegrationTest;
-import com.yandex.market.uploadservice.model.FileMetaInfo;
-import com.yandex.market.uploadservice.repository.FileMetaInfoRepository;
 import org.apache.commons.codec.digest.MurmurHash3;
-import org.checkerframework.checker.units.qual.A;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.yandex.market.auth.util.AuthUtils.token;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 class FileControllerTest extends UploadIntegrationTest {
@@ -57,27 +40,121 @@ class FileControllerTest extends UploadIntegrationTest {
     MockMvc mockMvc;
 
     @Test
-    public void test() throws Exception {
+    public void getUrlsWhenOk() throws Exception {
+        mockGenerateUrlMethod();
+
         mockMvc.perform(
-                        MockMvcRequestBuilders.multipart("/public/api/v1/files")
+                        MockMvcRequestBuilders.get("/public/api/v1/files")
                                 .accept(MediaType.APPLICATION_JSON)
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .queryParam("filesIds", "33f31181-d61d-4b7f-b4ae-9efa76f80a63")
+                                .queryParam("filesIds",
+                                                  "eb527df9-fac2-4de5-96ea-8c11ba8089f0",
+                                                  "2b8e3df0-628a-4ef0-9044-e14a995b539d",
+                                                  "cb25d05a-11f0-46c2-bc17-1a1185ade628")
                 )
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").value(uuid.toString()));
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("""
+                                                        [
+                                                            "https://www.instagram.com/1",
+                                                            "https://www.instagram.com/2",
+                                                            "https://www.instagram.com/3"
+                                                        ]
+                                                     """
+                ));
+
+        verify(amazonS3, times(3)).generatePresignedUrl(any(), any(), any());
+        verifyNoMoreInteractions(amazonS3);
     }
 
-    private JwtAuthenticationToken getToken() {
-        Jwt jwt = Jwt.withTokenValue("token")
-                .header("kid", "fe0d2248-137f-4810-b66b-e6d9b05213b5")
-                .header("alg", "RS256")
-                .claim("sub", "arkady@gmail.com")
-                .claim("seller-id", "0cd635af-2f23-4861-8441-5ebbbe4ae54a")
-                .claim("authorities", List.of("ROLE_SELLER"))
-                .build();
-        Collection<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("ROLE_SELLER");
-        return new JwtAuthenticationToken(jwt, authorities);
+    @Test
+    public void uploadWhenOk() throws Exception {
+        UUID uuid = UUID.fromString("62dc66f7-e141-4283-9c1d-a0dd0e2aba21");
+        List<MockMultipartFile> mockFiles = getMockedFiles();
+        mockPutMethod();
+        try (
+                MockedStatic<UUID> mockedUuid = Mockito.mockStatic(UUID.class);
+                MockedStatic<MurmurHash3> mockedMurMur = Mockito.mockStatic(MurmurHash3.class)
+        ) {
+            mockedUuid.when(UUID::randomUUID).thenReturn(uuid);
+            mockedMurMur.when(() -> MurmurHash3.hash32x86(mockFiles.get(0).getBytes())).thenReturn(0);
+            mockedMurMur.when(() -> MurmurHash3.hash32x86(mockFiles.get(1).getBytes())).thenReturn(1);
+            mockedMurMur.when(() -> MurmurHash3.hash32x86(mockFiles.get(2).getBytes())).thenReturn(2);
+            mockedMurMur.when(() -> MurmurHash3.hash32x86(mockFiles.get(3).getBytes())).thenReturn(3);
+
+            mockMvc.perform(
+                            MockMvcRequestBuilders.multipart("/public/api/v1/files")
+                                    .file(mockFiles.get(0))
+                                    .file(mockFiles.get(1))
+                                    .file(mockFiles.get(2))
+                                    .file(mockFiles.get(3))
+                                    .with(authentication(token("t51c4cd3-6fe7-4d3e-b82c-f5d044e46091", "ROLE_USER")))
+                                    .accept(MediaType.MULTIPART_FORM_DATA)
+                                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    )
+                    .andExpect(status().isCreated())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(content().json("""
+                                                            [
+                                                              "eb527df9-fac2-4de5-96ea-8c11ba8089f0",
+                                                              "2b8e3df0-628a-4ef0-9044-e14a995b539d",
+                                                              "cb25d05a-11f0-46c2-bc17-1a1185ade628",
+                                                              "62dc66f7-e141-4283-9c1d-a0dd0e2aba21"
+                                                            ]
+                                                         """
+                    ));
+        }
+
+        verify(amazonS3, times(3)).generatePresignedUrl(any(), any(), any());
+        verifyNoMoreInteractions(amazonS3);
+    }
+
+    private List<MockMultipartFile> getMockedFiles() throws IOException {
+        MockMultipartFile file0 = new MockMultipartFile(
+                "files",
+                "test_photo_0.jpeg",
+                null,
+                Files.readAllBytes(Paths.get("src/test/resources/photos/test_photo_0.jpeg")));
+
+        MockMultipartFile file1 = new MockMultipartFile(
+                "files",
+                "test_photo_1.jpg",
+                null,
+                Files.readAllBytes(Paths.get("src/test/resources/photos/test_photo_1.jpg")));
+
+        MockMultipartFile file2 = new MockMultipartFile(
+                "files",
+                "test_photo_2.png",
+                null,
+                Files.readAllBytes(Paths.get("src/test/resources/photos/test_photo_2.png")));
+
+        MockMultipartFile file3 = new MockMultipartFile(
+                "files",
+                "test_photo_3.jpg",
+                null,
+                Files.readAllBytes(Paths.get("src/test/resources/photos/test_photo_3.jpg")));
+
+        return List.of(file0, file1, file2, file3);
+    }
+
+    private void mockGenerateUrlMethod() throws MalformedURLException {
+        Mockito.doReturn(new URL("https://www.instagram.com/1"))
+                .when(amazonS3)
+                .generatePresignedUrl(any(), eq("eb527df9-fac2-4de5-96ea-8c11ba8089f0"), any());
+
+        Mockito.doReturn(new URL("https://www.instagram.com/2"))
+                .when(amazonS3)
+                .generatePresignedUrl(any(), eq("2b8e3df0-628a-4ef0-9044-e14a995b539d"), any());
+
+        Mockito.doReturn(new URL("https://www.instagram.com/3"))
+                .when(amazonS3)
+                .generatePresignedUrl(any(), eq("cb25d05a-11f0-46c2-bc17-1a1185ade628"), any());
+    }
+
+    private void mockPutMethod() {
+        Mockito.doReturn(null)
+                .when(amazonS3)
+                .putObject(any(), eq("62dc66f7-e141-4283-9c1d-a0dd0e2aba21"), any(), any());
     }
 
 }
