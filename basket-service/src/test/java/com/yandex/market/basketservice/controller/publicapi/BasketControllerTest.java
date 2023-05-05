c465b047f96b76ccda499d5e3a4ea0c8d4fb65bb
EgorovGrigorii <egorov.gr.pav@gmail.com>
2023-05-05 12:46:37 +0300

    create all tests

--- basket-service/src/test/java/com/yandex/market/basketservice/controller/publicapi/BasketControllerTest.java
+++ basket-service/src/test/java/com/yandex/market/basketservice/controller/publicapi/BasketControllerTest.java
@@ -1,17 +1,21 @@
 package com.yandex.market.basketservice.controller.publicapi;
 
-
 import com.yandex.market.basketservice.AbstractIntegrationTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static com.yandex.market.auth.util.AuthUtils.token;
 import static com.yandex.market.basketservice.models.Environment.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class BasketControllerTest extends AbstractIntegrationTest {
@@ -20,12 +24,110 @@ class BasketControllerTest extends AbstractIntegrationTest {
     private MockMvc mockMvc;
 
     @Test
+    @DisplayName("Получение информации о содержании корзины авторизованным пользователем")
     void whenUserWithTokenRequestInformationAboutBasket_thenOk() throws Exception {
         mockMvc.perform(
-                        get(BASKET_PATH, REAL_USER_ID, PageRequest.of(0, 10))
-                                .with(authentication(token(REAL_USER_ID,"ROLE_USER")))
-                                .header("Authorization", AUTH_TOKEN)
-                                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                        get(BASKET_PATH, EXISTING_USER_ID, PageRequest.of(0, 5))
+                                .with(authentication(token(EXISTING_USER_ID, "ROLE_USER")))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().json(Files.readString(Path.of(PATH_BASKET_RESPONSE))));
     }
+
+    @Test
+    @DisplayName("Попытка получения информации о корзине у неавторизованного пользователя")
+    void whenUserWithoutTokenTryToGetInformationAboutBasket_thenExpectUnauthorizedStatus() throws Exception {
+        mockMvc.perform(
+                        get(BASKET_PATH, EXISTING_USER_ID, PageRequest.of(0, 5))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("Авторизованный пользователь добавляет существующий товар в корзину")
+    void whenUserWithTokenTryToAddExistItemToBasket_thenOk() throws Exception {
+        mockMvc.perform(
+                        patch(BASKET_PATH, EXISTING_USER_ID)
+                                .with(authentication(token(EXISTING_USER_ID, "ROLE_USER")))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "productId" : "f34c4cd3-6fe7-4d3e-b82c-f5d044e46091",
+                                            "numberOfItems" : "7"
+                                        }
+                                        """)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().string("25"));
+    }
+
+    @Test
+    @DisplayName("Авторизованный пользователь пытается добавить существующий товар с отрицательным количеством в корзину")
+    void whenUserWithTokenTryToAddExistItemWithNegativeNumberOfItemsToBasket_thenBadRequest() throws Exception {
+        mockMvc.perform(
+                        patch(BASKET_PATH, EXISTING_USER_ID)
+                                .with(authentication(token(EXISTING_USER_ID, "ROLE_USER")))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "productId" : "f34c4cd3-6fe7-4d3e-b82c-f5d044e46091",
+                                            "numberOfItems" : "-10"
+                                        }
+                                        """)
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Авторизованный пользователь пытается добавить несуществующий товар в корзину")
+    void whenUserWithTokenTryToAddNotExistItemToBasket_thenBadRequest() throws Exception {
+        mockMvc.perform(
+                        patch(BASKET_PATH, EXISTING_USER_ID)
+                                .with(authentication(token(EXISTING_USER_ID, "ROLE_USER")))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "productId" : "f34c4cd3-6fe7-4d3e-b82c-f5d044e45555",
+                                            "numberOfItems" : "10"
+                                        }
+                                        """)
+                )
+                .andExpect(status().isNotFound());
+    }
+
+
+    @Test
+    @DisplayName("Авторизованный пользователь удаляет товар из корзины")
+    void whenUserWithTokenTryToRemoveItemFromBasket_thenOk() throws Exception {
+        mockMvc.perform(
+                        delete(BASKET_PATH, EXISTING_USER_ID)
+                                .with(authentication(token(EXISTING_USER_ID, "ROLE_USER")))
+                                .param("products", "f34c4cd3-6fe7-4d3e-b82c-f5d044e46091"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("18"));
+    }
+
+
+
+    /* Список тестов:
+     * 1) Существующий авторизованный пользователь с уже заполненной корзиной запрашивает информацию о своей корзине;
+     *   1.1) зарегистрированный пользователь без корзины пытается добавить товар в корзину
+     * 2) Этот юзер может добавить новый товар в корзину;
+     *   2.1) Юзер пробует добавить несуществующий товар;
+     *   2.2) Неавторизованный пользователь пытается добавить товар;
+     *   2.3) Юзер пытается добавить отрицательное или нулевое количество товара;
+     *   2.4) Юзер передаёт неверный формат ИД товара
+     * 3) Изменить количество уже добавленного товара;
+     *   3.1) Юзер пытается изменить количество товара на отрицательное значение, или на 0;
+     *   3.2) Передаётся неверный формат ИД товара;
+     * 4) Удалить добавленный товар;
+     *   4.1) Юзер пытется удалить несуществующий товар;
+     *   4.2) Клиент передаёт неверный формат ИД товара;
+     *   4.3) Клиент передаёт пустой список для удаления;
+     *
+     *  */
+
+
 }
--- basket-service/src/test/java/com/yandex/market/basketservice/models/Environment.java
+++ basket-service/src/test/java/com/yandex/market/basketservice/models/Environment.java
@@ -1,7 +1,5 @@
 package com.yandex.market.basketservice.models;
 
-import java.util.UUID;
-
 import static com.yandex.market.util.HttpUtils.PUBLIC_API_V1;
 
 public class Environment {
@@ -9,7 +7,8 @@ public class Environment {
     public static final String
             BASKET_PATH = "/" + PUBLIC_API_V1 + "/users/{userId}/basket/products";
 
-    public static final String REAL_USER_ID = "6a2e63a7-a8b7-4a5e-9422-6a16ee963e8d";
 
-    public static final String AUTH_TOKEN = "Bearer eyJraWQiOiI1OGY1NmJlNS01NTcxLTQzYzYtOGViMS0zMmFjNzJkMjMzY2UiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJyb2NrQHJvY2sucnUiLCJhdWQiOiJjbGllbnQiLCJuYmYiOjE2ODI2ODE3NzYsInNjb3BlIjpbIm9wZW5pZCJdLCJ1c2VyLWlkIjoiNmEyZTYzYTctYThiNy00YTVlLTk0MjItNmExNmVlOTYzZThkIiwiaXNzIjoiaHR0cDovLzUxLjI1MC4xMDIuMTI6OTAwMCIsImV4cCI6MTY4MzI4NjU3NiwiaWF0IjoxNjgyNjgxNzc2LCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXX0.q6nZS3ZBbtj2wIMEu7BU-w0EWkqBbF2UIlheqNPYuTOYDGE4igsgS4xFUESmDbK0YWv7ykbcaWBav6e61Y8KOmGZ7DBrIqiEnhW_wozX64tUBe5ZHonkTcnNwuetv4I5GCptIfK_SOy2PxeZDnEMNZTobtPRfRHNBAj4BwtbWudKRy2ieBH_4jHjsInhxMTWbQ1QY-O4VyY7k9uWb0oAMLRYsPaIhQuAKucYERgnU-HnPgEd1l61EG9yFxscrZq819hxuyjhx4FOyVtrbkRbwhBb93Dp4jA1jLJq32FEFfZVyeEpREnGGziVzn6_DP63_p2izfNjt8Gx9ED4QP5_aQ";
+    public static final String EXISTING_USER_ID = "6a2e63a7-a8b7-4a5e-9422-6a16ee963e8d";
+
+    public static final String PATH_BASKET_RESPONSE = "src/test/resources/json/BasketResponse.json";
 }
